789f137e1fdaed57c5c744b3b290e59bc82e18dd
rgboyd10 <rgboyd10@gmail.com>
2017-05-08 11:46:15 -0400

    Create ex2-5.cpp

---
+++ ex2-5.cpp
@@ -0,0 +1,12 @@
+/*
+Q - Write a set of "*" characters so that they form a square, triangle, and rectangle.
+A - 
+*/
+#include <iostream>
+#include <string>
+
+int main()
+{
+  int rows = 10;
+  for (int x = 0; x <= 
+}
