13abfbb5d7e5a05a47dd66c611af711cc885d55c
rgboyd10 <rgboyd10@gmail.com>
2017-05-08 12:00:42 -0400

    Update ex2-5.cpp

--- ex2-5.cpp
+++ ex2-5.cpp
@@ -1,12 +1,35 @@
 /*
 Q - Write a set of "*" characters so that they form a square, triangle, and rectangle.
-A - 
+A -  see below
 */
 #include <iostream>
 #include <string>
 
+//say which standard library names we use
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
 int main()
 {
-  int rows = 10;
-  for (int x = 0; x <= 
+  cout << "************";
+  cout << "*          *";
+  cout << "*          *";
+  cout << "*          *";
+  cout << "************";
+  
+  cout << "       *       ";
+  cout << "      * *      ";
+  cout << "     *   *     ";
+  cout << "    *     *    ";
+  cout << "   *********   ";
+  
+  cout << "  *********************** ";
+  cout << "  *                     * ";
+  cout << "  *                     * ";
+  cout << "  *                     * ";
+  cout << "  *********************** ";
+  
+  return 0;
 }
